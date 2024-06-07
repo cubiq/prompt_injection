798cddf090a87386f3dabc76c6532981855a53ba
cubiq <matteo@elf.io>
2024-06-07 10:41:31 +0200

    refactor the nodes

--- README.md
+++ README.md
@@ -1,38 +1,13 @@
 # Prompt Injection Node for ComfyUI
 
-This custom node for ComfyUI allows you to inject specific prompts at specific blocks of the Stable Diffusion UNet, providing fine-grained control over the generated image. It is based on the concept that the content/subject understanding of the model is primarily contained within the MID0 and MID1 blocks, as demonstrated in the B-Lora (Content Style implicit separation) paper.
-Features
+This custom node for ComfyUI allows you to inject specific prompts at specific blocks of the Stable Diffusion UNet, providing fine-grained control over the generated image.
 
-Inject different prompts into specific UNet blocks
-Three different node variations for flexible workflow integration
-Customize the learning rate of specific blocks to focus on content, lighting, style, or other aspects
-Potential for developing a "Mix of Experts" approach by swapping blocks on-the-fly based on prompt content
+## Highly Experimental
 
-# Usage
+The code is very basic, experimental and prossibly buggy. It's a very interesting proof of concept and I will expand it if anything good can be done with it. 
 
-Add the prompt_injection.py node to your ComfyUI custom nodes directory
-In your ComfyUI workflow, connect the desired node variation based on your input preferences
-Specify the prompts for each UNet block you want to customize
-Connect the output to the rest of your workflow and generate the image
+At the moment this is a fork of [DataCTE](https://github.com/DataCTE/prompt_injection)'s repository, I'm contact with them and we'll evaluate a merge when the code is stable.
 
-# Node Variations
+## Credits
 
-Prompt Injection (Single Prompt): Injects a single prompt into the specified UNet blocks
-Prompt Injection (Multiple Prompts): Allows injecting different prompts into each specified UNet block
-Prompt Injection (Prompt Dictionary): Accepts a dictionary of block names and their corresponding prompts
-
-# Example
-Injecting the prompt "white cat" into the OUTPUT0 and OUTPUT1 blocks, while using the prompt "blue dog" for all other blocks, results in an image with the composition of the "blue dog" prompt but with a cat as the subject/content.
-Acknowledgements
-
-Modified and simplified version of the node from: https://github.com/pamparamm/sd-perturbed-attention
-Inspired by discussions and findings shared by @Mobioboros and @DataVoid
-
-# Future Work
-
-Investigate the location of different concepts (e.g., lighting) within the UNet blocks
-Develop a "guts diagram" of the SDXL UNet to understand where each aspect is stored
-Explore the use of different learning rates for specific blocks during fine-tuning or LoRA training
-Implement a "Mix of Experts" approach by swapping blocks on-the-fly based on prompt content
-
-Feel free to contribute, provide feedback, and share your findings!
+This code is based on [DataCTE](https://github.com/DataCTE/prompt_injection), [Perturbed Attention](https://github.com/pamparamm/sd-perturbed-attention), [B-Lora](https://github.com/yardenfren1996/B-LoRA/) and my previous experiments with the [IPAdapter](https://github.com/cubiq/ComfyUI_IPAdapter_plus?tab=readme-ov-file) style/composition.
--- prompt_injection.py
+++ prompt_injection.py
@@ -1,8 +1,3 @@
-#Modified/simplified version of the node from: https://github.com/pamparamm/sd-perturbed-attention
-#If you want the one with more options see the above repo.
-
-#My modified one here is more basic but has less chances of breaking with ComfyUI updates.
-
 import comfy.model_patcher
 import comfy.samplers
 import torch
@@ -16,12 +11,19 @@ def build_patch(patchedBlocks, weight=1.0, sigma_start=0.0, sigma_end=1.0):
 
         if sigma <= sigma_start and sigma >= sigma_end:
             if (block and f'{block}:{block_index}' in patchedBlocks and patchedBlocks[f'{block}:{block_index}']):
-                c = context_attn1[0].unsqueeze(0)
-                b = patchedBlocks[f'{block}:{block_index}'][0][0].to(context_attn1.device)
-                if c.dim() == 2:
-                    c = c.unsqueeze(0)
-                out = torch.stack((c, b))
+                if context_attn1.dim() == 3:
+                    c = context_attn1[0].unsqueeze(0)
+                else:
+                    c = context_attn1[0][0].unsqueeze(0)
+                b = patchedBlocks[f'{block}:{block_index}'][0][0].repeat(c.shape[0], 1, 1).to(context_attn1.device)
+                #print(patchedBlocks[f'{block}:{block_index}'][0][0].shape)
+                #print(c.shape, b.shape, n.shape, context_attn1.shape)
+                out = torch.stack((c, b)).to(dtype=context_attn1.dtype) * weight
                 out = out.repeat(1, batch_prompt, 1, 1) * weight
+                #print(out.shape)
+
+                #out = torch.stack((c, b)).to(dtype=context_attn1.dtype)
+                #out = out.repeat(1, batch_prompt, 1, 1) * weight
 
                 return n, out, out 
 
@@ -36,17 +38,18 @@ class PromptInjection:
                 "model": ("MODEL",),
             },
             "optional": {
-                "in4":  ("CONDITIONING",),
-                "in5":  ("CONDITIONING",),
-                "in7":  ("CONDITIONING",),
-                "in8":  ("CONDITIONING",),
-                "mid0": ("CONDITIONING",),
-                "out0": ("CONDITIONING",),
-                "out1": ("CONDITIONING",),
-                "out2": ("CONDITIONING",),
-                "out3": ("CONDITIONING",),
-                "out4": ("CONDITIONING",),
-                "out5": ("CONDITIONING",),
+                "all":  ("CONDITIONING",),
+                "input_4":  ("CONDITIONING",),
+                "input_5":  ("CONDITIONING",),
+                "input_7":  ("CONDITIONING",),
+                "input_8":  ("CONDITIONING",),
+                "middle_0": ("CONDITIONING",),
+                "output_0": ("CONDITIONING",),
+                "output_1": ("CONDITIONING",),
+                "output_2": ("CONDITIONING",),
+                "output_3": ("CONDITIONING",),
+                "output_4": ("CONDITIONING",),
+                "output_5": ("CONDITIONING",),
                 "weight": ("FLOAT", {"default": 1.0, "min": -2.0, "max": 5.0, "step": 0.05}),
                 "start_at": ("FLOAT", {"default": 0.0, "min": 0.0, "max": 1.0, "step": 0.001}),
                 "end_at": ("FLOAT", {"default": 1.0, "min": 0.0, "max": 1.0, "step": 0.001}),
@@ -58,26 +61,24 @@ class PromptInjection:
 
     CATEGORY = "advanced/model"
 
-    def patch(self, model: comfy.model_patcher.ModelPatcher, in4=None, in5=None, in7=None, in8=None, mid0=None, out0=None, out1=None, out2=None, out3=None, out4=None, out5=None, weight=1.0, start_at=0.0, end_at=1.0):
+    def patch(self, model: comfy.model_patcher.ModelPatcher, all=None, input_4=None, input_5=None, input_7=None, input_8=None, middle_0=None, output_0=None, output_1=None, output_2=None, output_3=None, output_4=None, output_5=None, weight=1.0, start_at=0.0, end_at=1.0):
+        if not any((all, input_4, input_5, input_7, input_8, middle_0, output_0, output_1, output_2, output_3, output_4, output_5)):
+            return (model,)
+
         m = model.clone()
         sigma_start = m.get_model_object("model_sampling").percent_to_sigma(start_at)
         sigma_end = m.get_model_object("model_sampling").percent_to_sigma(end_at)
 
-        if any((in4, in5, in7, in8, mid0, out0, out1, out2, out3, out4, out5)):
-            patchedBlocks = {
-                'input:4': in4,
-                'input:5': in5,
-                'input:7': in7,
-                'input:8': in8,
-                'middle:0': mid0,
-                'output:0': out0,
-                'output:1': out1,
-                'output:2': out2,
-                'output:3': out3,
-                'output:4': out4,
-                'output:5': out5,
-            }
-            m.set_model_attn2_patch(build_patch(patchedBlocks, weight=weight, sigma_start=sigma_start, sigma_end=sigma_end))
+        patchedBlocks = {}
+        blocks = {'input': [4, 5, 7, 8], 'middle': [0], 'output': [0, 1, 2, 3, 4, 5]}
+
+        for block in blocks:
+            for index in blocks[block]:
+                value = locals()[f"{block}_{index}"] if locals()[f"{block}_{index}"] is not None else all
+                if value is not None:
+                    patchedBlocks[f"{block}:{index}"] = value
+
+        m.set_model_attn2_patch(build_patch(patchedBlocks, weight=weight, sigma_start=sigma_start, sigma_end=sigma_end))
 
         return (m,)
 
@@ -89,8 +90,7 @@ class SimplePromptInjection:
                 "model": ("MODEL",),
             },
             "optional": {
-                "block": (("output", "middle", "input"),),
-                "index": ("INT", {"default": 0, "min": 0, "max": 8, "step": 1}),
+                "block": (["input:4", "input:5", "input:7", "input:8", "middle:0", "output:0", "output:1", "output:2", "output:3", "output:4", "output:5"],),
                 "conditioning": ("CONDITIONING",),
                 "weight": ("FLOAT", {"default": 1.0, "min": -2.0, "max": 5.0, "step": 0.05}),
                 "start_at": ("FLOAT", {"default": 0.0, "min": 0.0, "max": 1.0, "step": 0.001}),
@@ -103,15 +103,15 @@ class SimplePromptInjection:
 
     CATEGORY = "advanced/model"
 
-    def patch(self, model: comfy.model_patcher.ModelPatcher, block, index, conditioning=None, weight=1.0, start_at=0.0, end_at=1.0):
-        if not conditioning:
+    def patch(self, model: comfy.model_patcher.ModelPatcher, block, conditioning=None, weight=1.0, start_at=0.0, end_at=1.0):
+        if conditioning is None:
             return (model,)
 
         m = model.clone()
         sigma_start = m.get_model_object("model_sampling").percent_to_sigma(start_at)
         sigma_end = m.get_model_object("model_sampling").percent_to_sigma(end_at)
 
-        m.set_model_attn2_patch(build_patch({f"{block}:{index}": conditioning}, weight=weight, sigma_start=sigma_start, sigma_end=sigma_end))
+        m.set_model_attn2_patch(build_patch({f"{block}": conditioning}, weight=weight, sigma_start=sigma_start, sigma_end=sigma_end))
 
         return (m,)
 
@@ -162,7 +162,7 @@ NODE_CLASS_MAPPINGS = {
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
-    "PromptInjection": "Inject Prompt in Attention",
-    "SimplePromptInjection": "Inject Prompt in Attention (simple)",
-    "AdvancedPromptInjection": "Inject Prompt in Attention (advanced)"
+    "PromptInjection": "Attn2 Prompt Injection",
+    "SimplePromptInjection": "Attn2 Prompt Injection (simple)",
+    "AdvancedPromptInjection": "Attn2 Prompt Injection (advanced)"
 }
